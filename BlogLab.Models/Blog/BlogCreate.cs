1621f6d1281dc21f34c4651fe943c075147b9d4a
tanishq <tanishqsinghai11@gmail.com>
2021-03-04 20:09:13 +0530

    Models project complete, Repository project created with Account Repository

---
+++ BlogLab.Models/Blog/BlogCreate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogLab.Models.Blog
+{
+    public class BlogCreate
+    {
+        public int BlogId { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [MinLength(8, ErrorMessage = "Must be 8-40 characters")]
+        [MaxLength(40, ErrorMessage = "Must be 8-40 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required")]
+        [MinLength(300, ErrorMessage = "Must be 300-3000 characters")]
+        [MaxLength(3000, ErrorMessage = "Must be 300-3000 characters")]
+        public string Content { get; set; }
+        public int? PhtoId { get; set; }
+
+
+    }
+}
+
---
+++ BlogLab.Repository/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using BlogLab.Models.Account;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BlogLab.Models;
+
+namespace BlogLab.Repository
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken);
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername, CancellationToken cancellationToken);
+    }
+
+}
